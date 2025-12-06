0853ee6387253186af672b0e7be7e5abcea53e47
Raghib Quader <raghib.quader@gmail.com>
2025-12-06 06:30:20 -0500

    Fix wording in install script

--- install.sh
+++ install.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
+# Install tmux if not present
 sudo apt install tmux
 
+# Install pqcli latest version from GitHub
 cd /tmp/
 git clone https://github.com/rr-/pq-cli.git
 cd pq-cli
 pip install --user .
 
+# Install and start the daemon
 sudo mv pqcli.service /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl enable pqcli
