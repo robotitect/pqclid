2494fa8afbe46cfe07a0ab533f47851aa97bb222
Raghib Quader <raghib.quader@gmail.com>
2025-12-23 10:52:16 +0600

    Get coordinates of all boxes

--- parse_capture.rb
+++ parse_capture.rb
@@ -1,11 +1,10 @@
-capture = File.open(ARGV[0]) 
-
 Coordinates = Struct.new(:row, :col)
 Box = Struct.new(:top_left, :top_right, :bot_left, :bot_right)
 
-Boxes = Array.new
+top_left_corners = []
+textboxes = []
 
-top_left_corners = Array.new
+capture = File.readlines(ARGV[0]) 
 
 capture.each_with_index do |line, row|
   indices_top_left = line.enum_for(:scan, /(?=┌)/).map do 
@@ -17,14 +16,33 @@ capture.each_with_index do |line, row|
   end
 end
 
-puts top_left_corners
+# Get text for all boxes
+top_left_corners.each_with_index do |coords, i|
+  puts "Box #{i + 1}"
 
-# Get coordinates for all boxes
-# Go down until get bot left corner
-# Go right until get top right corner
-# Have coordinates for the bot right corner now as well
+  first_row = coords.row
+  first_col = coords.col
 
-top_left_corners.each do |coords|
-  r, c = coords
+  # Go right until get top right corner
+  last_col = first_col + capture[first_row].split("").drop(first_col).find_index('┐')
+
+  # Go down until get bot left corner
+  last_row = first_row
+  (first_row...capture.size).each do |row|
+    char = capture[row].split("")[first_col]
+    if char == '└'
+        last_row = row
+        break
+    end
+  end
+  
+  # Extract each line based on corners
+  textbox = Array.new()
+  (first_row..last_row).each do |r|
+    textbox << capture[r][first_col..last_col]
+  end
+
+  # puts textbox
+  textboxes << textbox
 end
 
