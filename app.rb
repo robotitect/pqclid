ccbaea4e1965ec672f5d66f966934898d30892da
Raghib Quader <raghib.quader@gmail.com>
2025-12-29 23:20:21 +0600

    Refactoring

--- app.rb
+++ app.rb
@@ -6,48 +6,22 @@ require 'erb'
 require_relative 'parser'
 
 get '/' do
-  # Set window size to 150x10,000 for max information
-  resized_for_capture =
-    system('tmux', 'resize-window', '-t', 'pqcli', '-x', '150', '-y', '10000')
-
-  # Capture pane with the spoofed size
-  captured =
-    system('tmux', 'capture-pane', '-t', 'pqcli', '-pJ', out: '.capture')
-
-  # Reset the size so that `tmux a` to the session resizes to fit the terminal
-  resized_back_to_auto =
-    system('tmux', 'set-option', '-t', 'pqcli', 'window-size', 'largest')
-
-  capture_success = resized_for_capture and captured and resized_back_to_auto
-
-  filename = if capture_success
-               print 'Capture: SUCCESS! '
-               puts 'pqcli running, capture saved to .capture and .capture.old'
-               _ = system('cp', '.capture', '.capture.old')
-
-               '.capture'
-             else
-               print 'Capture FAILED. '
-               puts 'pqcli most likely not running, using previous .capture.old'
-
-               '.capture.old'
-             end
-
-  current_time = File.mtime(filename).strftime('%r [%F]')
+  filename, capture_success = Parser.capture_and_write_to_file
+  capture_time = File.mtime(filename).strftime('%r [%F]')
 
   parser = Parser.new(filename)
   parser.process
   pqcli_data = parser.data
 
   erb :home, locals: {
-    charsheet: pqcli_data[:character],
+    charsheet: pqcli_data[:charsheet],
     equipment: pqcli_data[:equipment],
     plot: pqcli_data[:plot],
     spellbook: pqcli_data[:spells],
     inventory: pqcli_data[:inventory],
     quests: pqcli_data[:quests],
     current_task: pqcli_data[:current_task],
-    current_time: current_time,
+    capture_time: capture_time,
     running: capture_success
   }
 end
--- parser.rb
+++ parser.rb
@@ -5,17 +5,42 @@ require 'roman-numerals'
 
 # Parser for PQCLI "screenshots"
 class Parser
-  HEADER_CHAR_SHEET = 'Character Sheet'
-  HEADER_EQUIPMENT = 'Equipment'
-  HEADER_PLOT_DEV = 'Plot Development'
-  HEADER_SPELL_BOOK = 'Spell Book'
-  HEADER_INVENTORY = 'Inventory'
-  HEADER_QUESTS = 'Quests'
+  CAPTURE_FILENAME_OUT_TEXT = {
+    true =>
+      ['.capture', 'Capture success! pqcli running, capture saved to .capture' +
+       ' and .capture.old'],
+    false =>
+      ['.capture.old', 'Capture failed (pqcli may not be running). Using ' +
+       'previous .capture.old']
+  }.freeze
+
+  HEADER = { charsheet: 'Character Sheet', equipment: 'Equipment',
+             plot: 'Plot Development', spells: 'Spell Book',
+             inventory: 'Inventory', quests: 'Quests' }.freeze
 
   CHAR_SHEET_INT_ATTRS = ['Level', 'STR', 'CON', 'DEX', 'INT', 'WIS', 'CHA',
                           'HP Max', 'MP Max', 'XP', 'XP Needed',
                           'XP Remaining'].freeze
   CHAR_SHEET_RATIONAL_ATTRS = ['XP (%)', 'Time Left (h)'].freeze
+
+  # rubocop:disable Style/MutableConstant
+  CHAR_SHEET_ATTR_TO_EMOJI = {
+    'Name' => '❝',
+    'Race' => '🏁',
+    'Class' => '✍️',
+    'Level' => '📈',
+    'STR' => '💪',
+    'CON' => '🩸',
+    'DEX' => '🤌',
+    'INT' => '🧠',
+    'WIS' => '🧓',
+    'CHA' => '😍',
+    'HP Max' => '❤️',
+    'MP Max' => '🪄'
+  }
+  CHAR_SHEET_ATTR_TO_EMOJI.default = ''
+  CHAR_SHEET_ATTR_TO_EMOJI.freeze
+  # rubocop:enable Style/MutableConstant
 
   TITLE_MATCHER = /([ \w]+)/
   TOP_LEFT_CORNER_MATCHER = /(?=┌)/
@@ -36,12 +61,90 @@ class Parser
   # completed and the completion percentage.
   CurrentTask = Struct.new(:text, :percent_completed)
 
-  # Contains all the parsed data from the 7 screen regions (6 textboxes + current
-  # task at the bottom).
-  PqData = Struct.new(:character, :equipment, :plot, :spells, :inventory, :quests,
-                      :current_task)
+  # Contains all the parsed data from the 7 screen regions (6 textboxes +
+  # current task at the bottom).
+  PqData = Struct.new(:charsheet, :equipment, :plot, :spells, :inventory,
+                      :quests, :current_task)
 
   attr_reader :data
+
+  def self.capture_and_write_to_file_sys
+    # Set window size to 150x10,000 for max information
+    resized_for_capture =
+      system('tmux', 'resize-window', '-t', 'pqcli', '-x', '150', '-y', '10000')
+
+    # Capture pane with the spoofed size
+    captured =
+      system('tmux', 'capture-pane', '-t', 'pqcli', '-pJ', out: '.capture')
+
+    # Reset the size so that `tmux a` to the session resizes to fit the terminal
+    resized_back_to_auto =
+      system('tmux', 'set-option', '-t', 'pqcli', 'window-size', 'largest')
+
+    resized_for_capture and captured and resized_back_to_auto
+  end
+
+  def self.capture_and_write_to_file
+    success = Parser.capture_and_write_to_file_sys
+    system('cp', '.capture', '.capture.old') if success
+
+    filename, out_text = CAPTURE_FILENAME_OUT_TEXT[success]
+    puts out_text
+
+    [filename, success]
+  end
+
+  def self.top_left_corners_from_line(line, row_number)
+    indices_top_left = line.enum_for(:scan, TOP_LEFT_CORNER_MATCHER).map do
+      Regexp.last_match.offset(0).first
+    end
+
+    indices_top_left.map do |col|
+      Coordinates.new(row: row_number, col: col)
+    end
+  end
+
+  def self.validate_textbox_header?(textbox, header)
+    textbox.first[TITLE_MATCHER].strip == header
+  end
+
+  def self.parse_percent_time_left(line)
+    split_line = line.strip.split
+
+    percent = split_line.first[NUM_MATCHER].to_r
+
+    time_left, time_unit =
+      if split_line.size == 2
+        TIME_MATCHER.match(split_line.last).captures
+      else
+        [0, 'm']
+      end
+
+    [percent, time_left.to_r, time_unit]
+  end
+
+  def self.calc_xp(xp_remaining, xp_percent)
+    xp_total_to_next_lvl =
+      (xp_remaining / ((100r - xp_percent)/100r)).round.to_i
+    xp_current = xp_total_to_next_lvl - xp_remaining.round.to_i
+    [xp_current, xp_total_to_next_lvl]
+  end
+
+  def self.convert_character_sheet(charsheet_data)
+    charsheet_data.each do |key, val|
+      if CHAR_SHEET_INT_ATTRS.include?(key)
+        charsheet_data[key] = val.to_i
+      elsif CHAR_SHEET_RATIONAL_ATTRS.include?(key)
+        charsheet_data[key] = val.to_r
+      end
+    end
+
+    Parser.add_emojis_to(charsheet_data)
+  end
+
+  def self.add_emojis_to(textbox_data)
+    textbox_data.transform_keys { "#{CHAR_SHEET_ATTR_TO_EMOJI[it]} #{it}".strip }
+  end
 
   def process
     data = process_capture
@@ -51,7 +154,8 @@ class Parser
 
     data[:current_task] = CurrentTask.new(
       text: current_task_lines.first.strip,
-      percent_completed: parse_percent_time_left(current_task_lines.last).first
+      percent_completed:
+        Parser.parse_percent_time_left(current_task_lines.last).first
     )
 
     @data = data
@@ -66,17 +170,17 @@ class Parser
   # rubocop:disable Metrics/MethodLength
   def textbox_data_symbol(textbox)
     case textbox.first[TITLE_MATCHER].strip # Title/header of textbox
-    when HEADER_CHAR_SHEET
-      [parse_character_sheet(textbox), :character]
-    when HEADER_EQUIPMENT
+    when HEADER[:charsheet]
+      [parse_character_sheet(textbox), :charsheet]
+    when HEADER[:equipment]
       [parse_equipment(textbox), :equipment]
-    when HEADER_PLOT_DEV
+    when HEADER[:plot]
       [parse_plot_development(textbox), :plot]
-    when HEADER_SPELL_BOOK
+    when HEADER[:spells]
       [parse_spell_book(textbox), :spells]
-    when HEADER_INVENTORY
+    when HEADER[:inventory]
       [parse_inventory(textbox), :inventory]
-    when HEADER_QUESTS
+    when HEADER[:quests]
       [parse_quests(textbox), :quests]
     end
   end
@@ -97,16 +201,10 @@ class Parser
     top_left_corners = []
 
     @capture.each_with_index do |line, row|
-      indices_top_left = line.enum_for(:scan, TOP_LEFT_CORNER_MATCHER).map do
-        Regexp.last_match.offset(0).first
-      end
-
-      indices_top_left.each do |col|
-        top_left_corners << Coordinates.new(row: row, col: col)
-      end
+      top_left_corners << Parser.top_left_corners_from_line(line, row)
     end
 
-    top_left_corners
+    top_left_corners.flatten
   end
 
   def textboxes(top_left_corners)
@@ -128,10 +226,6 @@ class Parser
     (first_row..last_row).map { |row| @capture[row][first_col..last_col] }
   end
 
-  def validate_textbox_header?(textbox, header)
-    textbox.first[TITLE_MATCHER].strip == header
-  end
-
   def filter_textbox(textbox)
     # 1st [1..-2]: Skip first and last lines (both are just '----------------')
     # 2nd [1..-2]: Skip first and last columns (both are just '|')
@@ -146,48 +240,26 @@ class Parser
     filter_textbox(textbox).select { it.size == 2 }.to_h
   end
 
-  def parse_percent_time_left(line)
-    split_line = line.strip.split
-
-    percent = split_line.first[NUM_MATCHER].to_r
-
-    time_left, time_unit =
-      if split_line.size == 2
-        TIME_MATCHER.match(split_line.last).captures
-      else
-        [0, 'm']
-      end
-
-    [percent, time_left.to_r, time_unit]
-  end
-
   def parse_experience_textbox(textbox)
     tb_xp = filter_textbox(textbox).select { it.size == 1 }.flatten
     xp_remaining = tb_xp.first[XP_REMAINING_MATCHER].to_i
 
-    xp_percent, xp_time_left, xp_time_unit = parse_percent_time_left(tb_xp.last)
+    xp_percent, xp_time_left, xp_time_unit =
+      Parser.parse_percent_time_left(tb_xp.last)
     [xp_remaining, xp_percent, xp_time_left, xp_time_unit]
   end
 
-  def calc_xp(xp_remaining, xp_percent)
-    xp_total_to_next_lvl =
-      (xp_remaining / ((100r - xp_percent)/100r)).round.to_i
-    xp_current = xp_total_to_next_lvl - xp_remaining.round.to_i
-    [xp_current, xp_total_to_next_lvl]
-  end
-
   def parse_experience(textbox)
-    xp_remaining, xp_pct, time_left, time_unit = parse_experience_textbox(textbox)
-    xp_current, xp_total_to_next_lvl = calc_xp(xp_remaining, xp_pct)
+    xp_remaining, xp_pct, time_left, time_unit =
+      parse_experience_textbox(textbox)
+    xp_current, xp_total_to_next_lvl = Parser.calc_xp(xp_remaining, xp_pct)
 
-    {
-      'XP' => xp_current,
+    { 'XP' => xp_current,
       'XP Needed' => xp_total_to_next_lvl,
       'XP Remaining' => xp_remaining,
       'XP (%)' => xp_pct,
       'Time Left (h)' => time_left,
-      'Time Unit' => time_unit
-    }
+      'Time Unit' => time_unit }
   end
 
   def filter_todo_list(textbox)
@@ -209,72 +281,27 @@ class Parser
     end
 
     percent_completed, time_left, time_unit =
-      parse_percent_time_left(filtered_tb.last)
+      Parser.parse_percent_time_left(filtered_tb.last)
 
     [tasks, percent_completed, time_left, time_unit]
   end
 
-  def convert_character_sheet(charsheet_data)
-    charsheet_data.each do |key, val|
-      if CHAR_SHEET_INT_ATTRS.include?(key)
-        charsheet_data[key] = val.to_i
-      elsif CHAR_SHEET_RATIONAL_ATTRS.include?(key)
-        charsheet_data[key] = val.to_r
-      end
-    end
-
-    add_emojis_to(charsheet_data)
-  end
-
-  def add_emojis_to(textbox_data)
-    textbox_data.transform_keys! do |key|
-      case key
-      when 'Name'
-        "❝ #{key}"
-      when 'Race'
-        "🏁 #{key}"
-      when 'Class'
-        "✍️ #{key}"
-      when 'Level'
-        "📈 #{key}"
-      when 'STR'
-        "💪 #{key}"
-      when 'CON'
-        "🩸 #{key}"
-      when 'DEX'
-        "🤌 #{key}"
-      when 'INT'
-        "🧠 #{key}"
-      when 'WIS'
-        "🧓 #{key}"
-      when 'CHA'
-        "😍 #{key}"
-      when 'HP Max'
-        "❤️ #{key}"
-      when 'MP Max'
-        "🪄 #{key}"
-      else
-        key
-      end
-    end
-  end
-
   def parse_character_sheet(textbox)
-    return unless validate_textbox_header?(textbox, HEADER_CHAR_SHEET)
+    return unless Parser.validate_textbox_header?(textbox, HEADER[:charsheet])
 
     char_data = parse_generic(textbox).merge(parse_experience(textbox))
 
-    convert_character_sheet(char_data)
+    Parser.convert_character_sheet(char_data)
   end
 
   def parse_equipment(textbox)
-    return unless validate_textbox_header?(textbox, HEADER_EQUIPMENT)
+    return unless Parser.validate_textbox_header?(textbox, HEADER[:equipment])
 
     parse_generic(textbox)
   end
 
   def parse_plot_development(textbox)
-    return unless validate_textbox_header?(textbox, HEADER_PLOT_DEV)
+    return unless Parser.validate_textbox_header?(textbox, HEADER[:plot])
 
     acts, percent_completed, time_left, time_unit = parse_todo_list(textbox)
 
@@ -287,7 +314,7 @@ class Parser
   end
 
   def parse_spell_book(textbox)
-    return unless validate_textbox_header?(textbox, HEADER_SPELL_BOOK)
+    return unless Parser.validate_textbox_header?(textbox, HEADER[:spells])
 
     parse_generic(textbox).transform_values { RomanNumerals.to_decimal(it) }
   end
@@ -297,14 +324,13 @@ class Parser
   end
 
   def parse_inventory(textbox)
-    return unless validate_textbox_header?(textbox, HEADER_INVENTORY)
+    return unless Parser.validate_textbox_header?(textbox, HEADER[:inventory])
 
     filtered_tb = filter_inventory(textbox)
 
-    inv_data = {
-      'Items' => parse_generic(textbox).transform_values(&:to_i),
-      'Encumbrance (%)' => parse_percent_time_left(filtered_tb.last).first
-    }
+    inv_data = { 'Items' => parse_generic(textbox).transform_values(&:to_i),
+                 'Encumbrance (%)' =>
+                    Parser.parse_percent_time_left(filtered_tb.last).first }
 
     inv_data['Inventory Spaces Filled'],
       inv_data['Inventory Spaces Max'] =
@@ -314,7 +340,7 @@ class Parser
   end
 
   def parse_quests(textbox)
-    return unless validate_textbox_header?(textbox, HEADER_QUESTS)
+    return unless Parser.validate_textbox_header?(textbox, HEADER[:quests])
 
     quests, percent_completed, time_left, time_unit = parse_todo_list(textbox)
 
