cd1da9684d744224eaec6ae1399c8877a7d6023e
Raghib Quader <raghib.quader@gmail.com>
2025-12-30 22:08:49 +0600

    Updated PqCliSys to handle the explicit socket

--- pq_cli_sys.rb
+++ pq_cli_sys.rb
@@ -3,6 +3,9 @@
 # Contains constants and methods dealing with capturing from the tmux session
 # and writing it to file.
 module PqCliSys
+  SOCKET = "#{Dir.home}/.tmux_socket".freeze
+  TMUX_PREFIX_CMDS = ['tmux', '-S', SOCKET].freeze
+
   CAPTURE_FILENAME_OUT_TEXT = {
     true =>
       ['.capture', 'Capture success! pqcli running, capture saved to ' \
@@ -14,16 +17,16 @@ module PqCliSys
 
   def capture_and_write_to_file_sys
     # Set window size to 150x10,000 for max information
-    resized_for_capture =
-      system('tmux', 'resize-window', '-t', 'pqcli', '-x', '150', '-y', '10000')
+    resized_for_capture = system(*TMUX_PREFIX_CMDS, 'resize-window', '-t',
+                                 'pqcli', '-x', '150', '-y', '10000')
 
     # Capture pane with the spoofed size
-    captured =
-      system('tmux', 'capture-pane', '-t', 'pqcli', '-pJ', out: '.capture')
+    captured = system(*TMUX_PREFIX_CMDS, 'capture-pane', '-t', 'pqcli', '-pJ',
+                      out: '.capture')
 
     # Reset the size so that `tmux a` to the session resizes to fit the terminal
-    resized_back_to_auto =
-      system('tmux', 'set-option', '-t', 'pqcli', 'window-size', 'largest')
+    resized_back_to_auto = system(*TMUX_PREFIX_CMDS, 'set-option', '-t',
+                                  'pqcli', 'window-size', 'largest')
 
     resized_for_capture and captured and resized_back_to_auto
   end
