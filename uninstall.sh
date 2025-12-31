ee63d40fc8f55e4961d7024893323c7e77fd408a
Raghib Quader <raghib.quader@gmail.com>
2025-12-31 21:54:30 +0600

    Fix socket name in unintall

--- uninstall.sh
+++ uninstall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 systemctl --user stop pqclid.service
 
-tmux -S "$HOME/.tmux_socket" kill-session -t pqcli 2>/dev/null || true
+tmux -S "$HOME/tmux_pqcli_socket" kill-session -t pqcli 2>/dev/null || true
 
 systemctl --user disable pqclid.service
 
@@ -10,5 +10,5 @@ rm -f -v ~/.config/systemd/user/pqclid.service
 systemctl --user daemon-reload
 systemctl --user reset-failed
 
-rm -f -v "$HOME/.tmux_socket"
+rm -f -v "$HOME/.tmux_pqcli_socket"
 rm -f -v "$HOME/.local/bin/tmux-pq-supervisor"
