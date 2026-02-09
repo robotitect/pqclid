320343d44ed8f62660e214c40ec9b43aac010687
Raghib Quader <raghib.quader@gmail.com>
2026-02-08 20:37:25 -0500

    Fix link for curl command

--- install.sh
+++ install.sh
@@ -158,7 +158,7 @@ if [ "$OS" = "Linux" ]; then
         cd "$TMP"
 
         log "Downloading ruby-build"
-        curl -fsSL https://github.com/ruby/ruby-build/archive/refs/heads/master.tar.gz | tar xz
+        curl -fsSL https://github.com/rbenv/ruby-build/archive/refs/tags/v20260121.tar.gz | tar xz
         cd ruby-build-master
         PREFIX="$APP_DIR/ruby/$RUBY_VERSION"
         ./bin/ruby-build "$RUBY_VERSION" "$PREFIX"
